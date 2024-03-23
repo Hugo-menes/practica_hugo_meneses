585ef6aa5d35216f94182b88e22f28e4c96b5b84
hugo_menes <hugo_meneses@usmp.pe>
2024-03-23 09:55:08 -0500

    iniciar commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace practica1_hugo_meneses.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
